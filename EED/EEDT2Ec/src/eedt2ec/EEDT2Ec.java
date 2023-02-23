244c7e034dbfd98032f62cdec789133895f79dff
Juan <Juan@DESKTOP-VL6HFIL>
2023-02-23 10:11:33 +0100

    saludito

---
+++ EED/EEDT2Ec/src/eedt2ec/EEDT2Ec.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eedt2ec;
+
+/**
+ *
+ * @author Juan
+ */
+public class EEDT2Ec {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es: Juan Herrera Haro, y soy alumno del ciclo 1º DAW del curso 2022/2023, mi DNI es 79800391D ");
+    }
+    
+}
